ce157f1cfcbc0c5fa11e38a6b18fd565bb2f1ba1
Max-M <maxnmagura@gmail.com>
2019-10-24 21:18:07 -0500

    tried to write some js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,18 @@
-$("click").dropdown('toggle')
+var friendsData = [
+  {
+  name: "Walter",
+  photo: "https://previews.123rf.com/images/opolja/opolja1901/opolja190101052/117091789-attractive-man-with-cigar-and-a-glass-whiskey.jpg",
+  scores:[5,1,4,4,5,1,2,5,4,1]
+},
+
+{
+  name: "Buddy",
+  photo: "https://www.featurepics.com/StockImage/20150606/man-drinking-beer-stock-image-3615070.jpg",
+  scores:[4,1,1,1,1,1,1,1,1,1]
+}
+
+];
+
+module.exports = friendsData;
+
+
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,41 @@
+
+var friendsData = require("./../data/friends");
+
+
+
+
+// console.log(friendsData)
+
+
+// var userInput = [1,5,4,5,3,2,2,1,3,5];
+// var testInput = [4,1,1,1,1,1,1,1,1,1];
+
+// var inputDifferenceArray = [];
+
+// for (i = 0; i < userInput.length; i++) {
+//   inputDifferenceArray.push(Math.abs(userInput[i] - testInput[i]))
+// }
+
+// function reduceFunction(total, num) {
+//   return Math.abs(total + num);
+// }
+
+// var inputDifferenceResult = inputDifferenceArray.reduce(reduceFunction)
+
+// //console.log(inputDifferenceResult)
+
+
+// // console.log(userInput)
+
+// var friendsScoresArray = []
+
+// for (var i = 0; i < friendsData.length; i++) {
+
+//   friendsScoresArray.push(friendsData[i].scores)
+
+//   var scoreDifference = []
+//   for (var j = 0; j < friendsScoresArray.length; j++) {
+//      scoreDifference.push(friendsScoresArray.reduce(reduceFunction))
+//   }
+  
+// }
